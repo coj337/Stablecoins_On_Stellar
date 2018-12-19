8abdabe11f0e247cfa7828cb781f022882ab9e01
Colin Watson <cojwatson@gmail.com>
2018-12-19 22:29:27 +1100

    - Finished bridge dockerfile - Implemented federation and compliance containers

--- Federation/federation.cfg.sh
+++ Federation/federation.cfg.sh
@@ -1,1 +1,9 @@
-﻿
+﻿echo port = ${FEDERATION_PORT}
+echo
+echo [database]
+echo type = "\"${DB_TYPE}\""
+echo dsn = "\"${DB_URI}\""
+echo
+echo [queries]
+echo federation = "\"SELECT '${RECEIVING_ACCOUNT}' as id, friendly_id as memo, 'text' as memo_type FROM accounts WHERE friendly_id = ? AND ? = '${NGINX_SERVER_NAME}'\""
+echo reverse-federation = "\"SELECT friendly_id, '' as domain FROM accounts WHERE ? = ''\""
